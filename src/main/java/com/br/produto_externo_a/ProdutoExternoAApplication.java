c4343b98c6c3868e62e6df67248af563155e3cea
Romário Cordeiro Cabó <romariocabo2012@gmail.com>
2025-06-04 12:36:00 -0300

    first commit

---
+++ src/main/java/com/br/produto_externo_a/ProdutoExternoAApplication.java
@@ -0,0 +1,13 @@
+package com.br.produto_externo_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoExternoAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutoExternoAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/produto_externo_a/ProdutoExternoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.produto_externo_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoExternoAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
